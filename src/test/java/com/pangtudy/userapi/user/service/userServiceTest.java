a759b006501ae0c7c41d05821eae81229aaf7794
corang0 <kmc546@naver.com>
2021-10-10 22:55:32 +0900

    feat: 회원가입, 사용자 조회 API 테스트 코드 추가
    각 API의 Controller, Service 테스트 코드 추가

---
+++ src/test/java/com/pangtudy/userapi/user/service/userServiceTest.java
@@ -0,0 +1,55 @@
+package com.pangtudy.userapi.user.service;
+
+import com.pangtudy.userapi.user.config.UserRole;
+import com.pangtudy.userapi.user.model.UserEntity;
+import com.pangtudy.userapi.user.model.UserResult;
+import com.pangtudy.userapi.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+public class userServiceTest {
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @DisplayName("사용자 목록 조회")
+    @Test
+    void findAll() {
+        // given
+        doReturn(userList()).when(userRepository).findAll();
+
+        // when
+        final List<UserResult> userList = (List<UserResult>) userService.getAll();
+
+        // then
+        assertThat(userList.size()).isEqualTo(2);
+    }
+
+    private Object userList() {
+        final List<UserEntity> userList = new ArrayList<>();
+
+        userList.add(new UserEntity((long)1, "test@test.test", "test", "pw1", UserRole.ROLE_USER, "1"));
+        userList.add(new UserEntity((long)2, "test2@test.test", "test2", "pw2", UserRole.ROLE_USER, "2"));
+
+        return userList;
+    }
+
+}
