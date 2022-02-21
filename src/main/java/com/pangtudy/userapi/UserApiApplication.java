e71fc189d96fca811b832f90e827ecc77e33d39c
corang0 <kmc546@naver.com>
2022-02-21 22:17:57 +0900

    feat: Cors 설정 추가

--- src/main/java/com/pangtudy/userapi/UserApiApplication.java
+++ src/main/java/com/pangtudy/userapi/UserApiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UserApiApplication {
@@ -18,4 +20,13 @@ public class UserApiApplication {
         SpringApplication.run(UserApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
