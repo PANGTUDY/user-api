ee81c0ec1ae9eeccfd410377a5b5214b9c8626a1
corang0 <kmc546@naver.com>
2022-02-21 22:11:00 +0900

    feat: Swagger 추가

--- src/main/java/com/pangtudy/userapi/user/config/SecurityConfig.java
+++ src/main/java/com/pangtudy/userapi/user/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
+                .antMatchers("/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs","/webjars/**", "/webjars/springfox-swagger-ui/*.{js,css}").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
