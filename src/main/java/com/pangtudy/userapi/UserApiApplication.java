07b06f27e0a937b2a3d69ff666c3049c319fb27d
corang0 <kmc546@naver.com>
2022-05-22 23:38:56 +0900

    feat: 설정 파일 분할, MySQL, Eureka 연동

--- src/main/java/com/pangtudy/userapi/UserApiApplication.java
+++ src/main/java/com/pangtudy/userapi/UserApiApplication.java
@@ -4,11 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableEurekaClient
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class UserApiApplication {
 
--- src/main/java/com/pangtudy/userapi/user/model/UserEntity.java
+++ src/main/java/com/pangtudy/userapi/user/model/UserEntity.java
@@ -10,27 +10,33 @@ import javax.persistence.*;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "user")
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Setter
+    @Column(name = "email")
     private String email;
 
     @Setter
+    @Column(name = "name")
     private String name;
 
     @Setter
+    @Column(name = "password")
     private String password;
 
     @Setter
-    @Column(name = "role")
     @Enumerated(EnumType.STRING)
+    @Column(name = "role")
     private UserRole role = UserRole.ROLE_NOT_PERMITTED;
 
     @Setter
+    @Column(name = "salt")
     private String salt;
 
 }
